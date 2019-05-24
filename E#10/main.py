b3a4ec56c544df426cc7d2e97e920bac69350a5d
repen <9keepa@gmail.com>
2019-05-24 23:00:20 +0500

    Added code from video E#10

---
+++ E#10/main.py
@@ -0,0 +1,38 @@
+'''
+name: E#10
+author: Andrey Plugin
+email: 9keepa@gmail.com
+link: https://www.youtube.com/channel/UCNN3bpPlWWUkUMB7gjcUFlw
+'''
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import unquote, quote_plus
+
+base_url = "https://useraudio.net/search/"
+
+query = quote_plus("prodigy")
+
+response = requests.get(base_url + query)
+
+html = response.text
+# print(html)
+
+soup = BeautifulSoup(html, "html.parser")
+
+songs = soup.select("div.title_wrap[id]")
+
+all_songs = []
+for song in songs:
+    url = "https://useraudio.net/?do=getById&id=" + song["data-songid"]
+    name = song.select("span.title-name")[0].text + ".mp3"
+    all_songs.append([url, name])
+
+for e, song in enumerate(all_songs):
+    if e == 5:
+        break
+    with open(song[1], "wb") as f:
+        response = requests.get(song[0]).content
+        f.write(response)
+
+
+
---
+++ E#10/temp.py
@@ -0,0 +1,36 @@
+'''
+name: E#10
+author: Andrey Plugin
+email: 9keepa@gmail.com
+link: https://www.youtube.com/channel/UCNN3bpPlWWUkUMB7gjcUFlw
+'''
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import unquote, quote_plus
+
+base_url = "https://useraudio.net/search/"
+
+query = quote_plus("5fiesta")
+
+response = requests.get(base_url + query)
+
+html = response.text
+# print(html)
+
+soup = BeautifulSoup(html, "html.parser")
+
+song_ids = soup.select("div[id].title_wrap")
+
+for e, s_id in enumerate(song_ids):
+    if e == 5:
+        break
+    name_song = s_id.select("span.title-name")[0].text
+    with open("{}.mp3".format(name_song), "wb") as f:
+        song_url = "https://useraudio.net/?do=getById&id={}".format(s_id["data-songid"])
+        content = requests.get(song_url).content
+        print("\t length: %10.2f МБ" % (len(content) / 2**20))
+        f.write(content)
+    print("Status parse: {}".format(len(song_ids) - e))
+    print("\t Download: {}.mp3".format(name_song))
+
+
