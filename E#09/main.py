b6071936d534044406fd2431d93940f04b388f7b
repen <9keepa@gmail.com>
2019-05-24 22:46:51 +0500

    Added code from video E#09

---
+++ E#09/main.py
@@ -0,0 +1,34 @@
+'''
+name: E#09
+author: Andrey Plugin
+email: 9keepa@gmail.com
+link: https://www.youtube.com/channel/UCNN3bpPlWWUkUMB7gjcUFlw
+'''
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "https://wallpapershome.com"
+
+space = "/space/?page=4"
+
+response = requests.get(base_url + space)
+
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+conteiner = soup.find("div", {"class":"pics"})
+
+images = conteiner.find_all("p")
+
+print(len(images))
+
+urls_images = []
+for image in images:
+    id_image = image.a["href"].split("-")[-1].replace(".html","")
+    urls_images.append("https://wallpapershome.com/images/pages/pic_h/{}.jpg".format(id_image))
+
+for e, url in enumerate(urls_images, start = 1):
+    image = requests.get(url).content
+    with open("image_{}.jpg".format(e), "wb") as f:
+        f.write(image)
---
+++ E#09/temp.py
@@ -0,0 +1,39 @@
+'''
+name: E#09
+author: Andrey Plugin
+email: 9keepa@gmail.com
+link: https://www.youtube.com/channel/UCNN3bpPlWWUkUMB7gjcUFlw
+'''
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "https://wallpapershome.com"
+space = "/space?page=4"
+
+response = requests.get(base_url + space)
+
+html = response.text
+
+# print(html)
+soup = BeautifulSoup(html, "html.parser")
+
+conteiner = soup.find("div", {"class":"pics"})
+
+images = conteiner.find_all("p")
+
+urls_images = []
+for image in images:
+    id_img = image.a["href"].split("-")[-1].replace(".html","")
+    urls_images.append("https://wallpapershome.com/images/pages/pic_h/" + id_img + ".jpg")
+    # break
+
+images_byte = []
+for url in urls_images:
+    images = requests.get(url)
+    images_byte.append(images.content)
+    # break
+
+for e, image in enumerate(images_byte):
+    # print(image)
+    with open("image{}.jpg".format(e), "wb") as f:
+        f.write(image)
