5d853d39c92d4665b0528b090f8c4e9da399e13a
repen <9keepa@gmail.com>
2019-08-25 20:10:11 +0500

    refactoring code E#15

--- E#15/database.py
+++ E#15/database.py
@@ -1,16 +1,20 @@
 import sqlite3
 
 conn = sqlite3.connect("mydata.db")
-# sql = "CREATE TABLE books (name TEXT, price TEXT, description TEXT, info TEXT)"
+
+sql = "CREATE TABLE books (name TEXT, price TEXT, description TEXT, info TEXT)"
 sql = "SELECT * FROM books"
 cursor = conn.cursor()
-cursor.execute(sql)
-res = cursor.fetchall()
-# print(res)
-for r in res:
-    print("Name:", r[0], "\nPrice: " ,r[1])
-conn.close()
 
+cursor.execute(sql)
+
+res = cursor.fetchall()
+
+for r in res:
+    print("Name:", r[0])
+    print("Price:", r[1])
+
+conn.close()
 
 
 
--- E#15/main.py
+++ E#15/main.py
@@ -9,6 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
+
 base_url = "http://books.toscrape.com/catalogue"
 
 response = requests.get(url)
@@ -19,13 +20,12 @@ soup = BeautifulSoup(html, "html.parser")
 
 container = soup.select_one("ol.row")
 
-elements = container.find_all("li")
+products = container.find_all("li")
 
 urls = []
-
-for element in elements:
-    url = element.select_one("h3 a")["href"]
-    urls.append(base_url + "/" + url.replace("../",""))
+for product in products:
+    url = product.select_one("h3 a")["href"]
+    urls.append(base_url + "/" +url.replace("../", ""))
 
 args = []
 for url in urls:
@@ -33,13 +33,19 @@ for url in urls:
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
     name = soup.select_one("h1").text
-    price =  soup.select_one("p.price_color").text[2:]
-    description =  soup.find("div", {"class":"sub-header"}).find_next("p").text
+    price = soup.select_one("p.price_color").text[2:]
+    description = soup.find("div", {"class":"sub-header"}).find_next("p").text
     info = str(soup.select_one("table.table.table-striped"))
     args.append((name, price, description, info))
 
 conn = sqlite3.connect("mydata.db")
+
 cursor = conn.cursor()
-cursor.executemany("INSERT INTO books VALUES(?,?,?,?)", args)
+
+cursor.executemany("INSERT INTO books VALUES (?,?,?,?)", args)
 conn.commit()
 conn.close()
+
+
+
+
